e68069349dc613672a1e1137634dad0124895c2b
Andrew Janke <andrew@apjanke.net>
2021-03-26 00:23:57 -0400

    Label Python operations

--- python/benchytimey.py
+++ python/benchytimey.py
@@ -11,7 +11,7 @@ def tic():
 def toc(t0):
   return time.time() - t0
 
-def formatEtime(te):
+def format_etime(te):
   st = '%.09f' % (te)
   ix = st.index('.')
   return st[0:ix] + '.' + st[ix+1:ix+4] + '_' + st[ix+4:ix+7] + '_' + st[ix+7:ix+10]
@@ -19,7 +19,7 @@ def formatEtime(te):
 fmt = '%-45s %s s'
 
 def say(msg, te):
-  print(fmt % (msg, formatEtime(te)))
+  print(fmt % (msg + " (Python):", format_etime(te)))
 
 # Current raw time in UTC, raw
 
@@ -27,7 +27,7 @@ t0 = tic()
 for i in range(0, N):
   blah = time.time()
 te = toc(t0)
-say('current raw UTC time:', te/N)
+say('current raw UTC time', te/N)
 
 # Current system time in local zone
 
@@ -35,7 +35,7 @@ t0 = tic()
 for i in range(0, N):
   dt = datetime.now()
 te = toc(t0)
-say('current zoned local time:', te/N)
+say('current zoned local time', te/N)
 
 # Current system time in local zone
 
@@ -43,7 +43,7 @@ t0 = tic()
 for i in range(0, N):
   dt = datetime.utcnow()
 te = toc(t0)
-say('current zoned UTC time:', te/N)
+say('current zoned UTC time', te/N)
 
 # UTC raw time to object
 
@@ -53,4 +53,4 @@ t0 = tic()
 for i in range(0, N):
   dt = datetime.utcfromtimestamp(someRandomPosixTimestamp)
 te = toc(t0)
-say('UTC raw time to object:', te/N)
+say('UTC raw time to object', te/N)
